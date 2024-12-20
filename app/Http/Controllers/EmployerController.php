00daaf93380fc53ddfdf81c07e82e38584a3fef9
Alish Shrestha <cresthaalish@gmail.com>
2024-12-20 17:29:14 +0545

    feat - get employer

--- app/Http/Controllers/EmployerController.php
+++ app/Http/Controllers/EmployerController.php
@@ -6,5 +6,26 @@ use Illuminate\Http\Request;
 
 class EmployerController extends Controller
 {
-    //
+    public function getEmployer()
+    {
+        try {
+            $employer = request()->user()->employer;
+            if (!$employer) {
+                return response()->json([
+                    'status' =>
+                    'fail',
+                    'error' => 'No employer found.',
+                ], 400);
+            }
+            return response()->json([
+                'status' => 'success',
+                'employer' => $employer
+            ], 200);
+        } catch (\Exception $e) {
+            return response()->json([
+                'status' => 'fail',
+                'error' => $e->getMessage(),
+            ], 500);
+        }
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\EmployerController;
 use App\Http\Controllers\JobController;
 use App\Http\Controllers\SearchController;
 use Illuminate\Http\Request;
@@ -35,3 +36,5 @@ Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
 Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+
+Route::get('/employer', [EmployerController::class, 'getEmployer'])->middleware('auth:sanctum');
